6e12b5087f579dad6dba760f5208565c58eb1148
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-09-11 15:49:51 +0000

    feat(components): implement reusable SideNav component

--- packages/components/src/shared/index.tsx
+++ packages/components/src/shared/index.tsx
@@ -1,3 +1,4 @@
 export { BreadcrumbCommon } from "./BreadcrumbCommon";
 export { LanguageSwitcher } from "./LanguageSwitcher";
 export { Spinner } from "./Spinner";
+export { SideNav, type SideNavItem } from "./SideNav";
