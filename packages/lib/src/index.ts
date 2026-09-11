c7c693706725b24aadbe5076354dfdf047cfdbb7
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-09-11 21:40:22 +0000

    ref(lib): centralize setNestedValue utility in lib package

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -2,3 +2,4 @@ export { delay } from "./delay.js";
 export { getLocaleFromEnv, getDateFnsLocale } from "./language.js";
 export { identifyUser, identifyUserAvatar } from "./user.js";
 export type { IdentifiableUser } from "./user.js";
+export { setNestedValue } from "./store.js";
