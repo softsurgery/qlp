a88167073773d815b62faa00947a73359f58d307
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-09-12 13:55:06 +0000

    fix(data-table): repair persistance of datatable state using useLocalStorage hook

--- packages/hooks/src/utils/index.ts
+++ packages/hooks/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { useDebounce } from "./useDebounce.js";
 export { useUploadSrc } from "./useUploadSrc.js";
 export type { UploadSrcApi, UploadSrcRef } from "./useUploadSrc.js";
+export { useLocalStorage } from "./useLocalStorage.js";
